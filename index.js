6a243b881d16d6e2b400c375b20592ae58914464
FarabyKhan <farabykhan346@gmail.com>
2025-11-11 22:16:19 +0600

    first commit of Ass-10-billease-server

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config()
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@am.7mxwxuq.mongodb.net/?appName=AM`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('billeaseDBUser')
+    const billsCollection = db.collection('bills')
+
+    app.get('/bills', async(req ,res)=>{
+ const result = await billsCollection.find().toArray()
+
+      res.send(result)
+    })
+
+    app.get('/bills/:id', async(req , res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await billsCollection.findOne(query)
+      res.send(result);
+    })
+
+    app.get('/latest-bills', async(req, res)=>{
+      const cursor = billsCollection.find().sort({date: -1}).limit(6)
+      const result = await cursor.toArray();
+      res.send(result)
+
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
